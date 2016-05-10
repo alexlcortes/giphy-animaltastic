f9c468d35b86c52e898f11c2a31be1816f85ec32
Alejandro Cortes <Alejandro Cortes>
2016-05-10 19:04:44 -0400

    mostly all of JS but it still doesn't work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,24 @@
-var animals = ['lion', 'cheetah', 'hippo', 'turtle', 'giraff'];
+var animals = ['lion', 'cheetah', 'hippo', 'turtle'];
 
-function displayAnimals() {
+function displayAnimals(){
 
 	var animal = $(this).attr('data-name');
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC";
 
 	$.ajax({url: queryURL, method: 'GET'}).done(function(response) {
 		$("#gif-results").html(JSON.stringify(response));
+		var results = response.data;
+		console.log(results)
 	});
 }
 
-function renderButtons () {
+function renderButtons() {
 
 	$('#the-buttons').empty();
 
 	for (var i = 0; i < animals.length; i++) {
 
-		var a = $('#the-buttons')
+		var a = $('<button>')
 
 		a.addClass('new-animal');
 		a.attr('data-name', animals[i]);
@@ -24,3 +26,18 @@ function renderButtons () {
 		$('#the-buttons').append(a);
 	}
 }
+
+$('#add-animal').on('click', function(){
+
+	var nextAnimal = $('#animal-input').val().trim();
+	animals.push(nextAnimal);
+
+	renderButtons();
+
+	return false;
+
+})
+
+$(document).on('click', '.new-animal', displayAnimals);
+
+	renderButtons();
